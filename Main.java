6d02b664f9aca40e60ae8f5b8ef5bfca3db0b635
saurabh24gope <104162109+saurabh24gope@users.noreply.github.com>
2024-01-07 22:42:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("This is java Program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
